191be63a57cd35b124560d8f019a11002712ffa7
Harivonjy Rica <rica.harivonjy@brainsolutions.be>
2023-06-30 08:52:50 +0300

    first commit

--- src/components/SayHello.jsx
+++ src/components/SayHello.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function SayHello() {
     return (
-        <h1 className="text-3xl text-amber-500 font-bold underline">Hello world</h1>
+        <h1 className="text-3xl text-amber-500 font-bold underline">
+            Hello world
+        </h1>
     );
 }
 
